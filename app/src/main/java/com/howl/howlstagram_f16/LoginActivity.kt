a97c3819852a874b2a768fb685b8540107b9aae7
Myeongsic <you6878@icloud.com>
2019-01-27 14:45:03 +0900

    Android API 28 And Firebase Version 16 - 2 Days

--- app/src/main/java/com/howl/howlstagram_f16/LoginActivity.kt
+++ app/src/main/java/com/howl/howlstagram_f16/LoginActivity.kt
@@ -1,12 +1,55 @@
 package com.howl.howlstagram_f16
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+            task ->
+                if(task.isSuccessful){
+                    //Creating a user account
+                    moveMainPage(task.result.user)
+                }else if(task.exception?.message.isNullOrEmpty()){
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }else{
+                    //Login if you have account
+                    signinEmail()
+                }
+        }
+    }
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result.user)
+                }else{
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
