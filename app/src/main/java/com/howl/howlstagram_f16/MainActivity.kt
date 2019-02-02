1e99cc197d0f4027d3a8ad4d067f8ba11dc16831
Myeongsic <you6878@icloud.com>
2019-02-02 15:43:09 +0900

    Android API 28 And Firebase Version 16 - 5 Day

--- app/src/main/java/com/howl/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/howl/howlstagram_f16/MainActivity.kt
@@ -2,11 +2,48 @@ package com.howl.howlstagram_f16
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import com.howl.howlstagram_f16.navigation.AlarmFragment
+import com.howl.howlstagram_f16.navigation.DetailViewFragment
+import com.howl.howlstagram_f16.navigation.GridFragment
+import com.howl.howlstagram_f16.navigation.UserFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home ->{
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
 }
---
+++ app/src/main/java/com/howl/howlstagram_f16/navigation/AlarmFragment.kt
@@ -0,0 +1,16 @@
+package com.howl.howlstagram_f16.navigation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.howl.howlstagram_f16.R
+
+class AlarmFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
+        return view
+    }
+}
