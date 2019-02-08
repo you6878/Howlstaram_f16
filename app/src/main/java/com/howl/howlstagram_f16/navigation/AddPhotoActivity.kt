6231a1b92b5eda98ccb3a448cc04279121eda77f
Myeongsic <you6878@icloud.com>
2019-02-08 14:33:19 +0900

    Android API 28 And Firebase Version 16 - 7 Day

--- app/src/main/java/com/howl/howlstagram_f16/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/howl/howlstagram_f16/navigation/AddPhotoActivity.kt
@@ -6,8 +6,13 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.howl.howlstagram_f16.R
+import com.howl.howlstagram_f16.navigation.model.ContentDTO
 import kotlinx.android.synthetic.main.activity_add_photo.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,12 +21,16 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    var auth : FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
-        //Initiate storge
+        //Initiate
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         //Open the album
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -57,9 +66,60 @@ class AddPhotoActivity : AppCompatActivity() {
 
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
-        //FileUpload
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        //Promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            //Insert downloadUrl of image
+            contentDTO.imageUrl = uri.toString()
+
+            //Insert uid of user
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //Insert userId
+            contentDTO.userId = auth?.currentUser?.email
+
+            //Insert explain of content
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            //Insert timestamp
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
         }
+
+        /*//Callback method
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var contentDTO = ContentDTO()
+
+                //Insert downloadUrl of image
+                contentDTO.imageUrl = uri.toString()
+
+                //Insert uid of user
+                contentDTO.uid = auth?.currentUser?.uid
+
+                //Insert userId
+                contentDTO.userId = auth?.currentUser?.email
+
+                //Insert explain of content
+                contentDTO.explain = addphoto_edit_explain.text.toString()
+
+                //Insert timestamp
+                contentDTO.timestamp = System.currentTimeMillis()
+
+                firestore?.collection("images")?.document()?.set(contentDTO)
+
+                setResult(Activity.RESULT_OK)
+
+                finish()
+            }
+        }*/
     }
 }
---
+++ app/src/main/java/com/howl/howlstagram_f16/navigation/model/ContentDTO.kt
@@ -0,0 +1,14 @@
+package com.howl.howlstagram_f16.navigation.model
+
+data class ContentDTO(var explain : String? = null,
+                      var imageUrl : String? = null,
+                      var uid : String? = null,
+                      var userId : String? = null,
+                      var timestamp : Long? = null,
+                      var favoriteCount : Int = 0,
+                      var favorites : Map<String,Boolean> = HashMap()){
+    data class Comment(var uid : String? = null,
+                       var userId : String? = null,
+                       var comment : String? = null,
+                       var timestamp : Long? = null)
+}
