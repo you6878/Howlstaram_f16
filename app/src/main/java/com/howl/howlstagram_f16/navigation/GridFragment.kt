50e4dd03cc2a082d4a6227033dd689f48f525f31
Myeongsic <you6878@icloud.com>
2019-02-22 13:06:49 +0900

    Android API 28 And Firebase Version 16 - 13 Day

--- app/src/main/java/com/howl/howlstagram_f16/navigation/GridFragment.kt
+++ app/src/main/java/com/howl/howlstagram_f16/navigation/GridFragment.kt
@@ -2,15 +2,66 @@ package com.howl.howlstagram_f16.navigation
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import com.howl.howlstagram_f16.R
+import com.howl.howlstagram_f16.navigation.model.ContentDTO
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 class GridFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
+    var fragmentView : View? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
-        return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        firestore = FirebaseFirestore.getInstance()
+        fragmentView?.gridfragment_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.gridfragment_recyclerview?.layoutManager = GridLayoutManager(activity,3)
+        return fragmentView
+    }
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        init {
+            firestore?.collection("images")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                //Sometimes, This code return null of querySnapshot when it signout
+                if(querySnapshot == null) return@addSnapshotListener
+
+                //Get data
+                for(snapshot in querySnapshot.documents){
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+
+            var imageview = ImageView(p0.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var imageview = (p0 as CustomViewHolder).imageview
+            Glide.with(p0.itemView.context).load(contentDTOs[p1].imageUrl).apply(RequestOptions().centerCrop()).into(imageview)
+        }
+
     }
 }
